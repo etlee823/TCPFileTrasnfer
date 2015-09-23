11b81630c9830751f3ea3495114d7cc5cbaede57
David Bui <davidbui17@hotmail.com>
2015-09-23 13:33:59 -0400

    Fixed formating issues

--- client.c
+++ client.c
@@ -1,18 +1,20 @@
-#include <stdio.h>			// For printf() and fprintf().
+#include <stdio.h>				// For printf() and fprintf().
 #include <sys/socket.h>		// For socket(), connect(), send(), recv().
 #include <arpa/inet.h>		// For sockaddr_in, inet_addr().
-#include <stdlib.h>			// For atoi().
-#include <string.h>			// For memset(), strstr().
-#include <unistd.h>			// For close(), access(), exec().
+#include <stdlib.h>				// For atoi().
+#include <string.h>				// For memset(), strstr().
+#include <unistd.h>				// For close(), access(), exec().
 
-#define RCVBUFSIZE 1024		// Size of receive message buffer.
-#define CMDBUFSIZE 1024		// Size of command buffer.
-#define DEBUG 0				// If defined, print statements will be enabled for debugging.
+#define BUFSIZE	1024			// Buffer size.
+//#define DEBUG 0						// If defined, print statements will be enabled for debugging.
 
 
 /////////////////////////////////////////////////////////////////////
 // Function protoypes.
 /////////////////////////////////////////////////////////////////////
+
+// Exits the program with error an message
+void Die(const char *message) { perror(message); exit(1); }
 
 // Returns 0 if a string starts with a specfified substring.
 int StartsWith(const char *string, const char *substring);
@@ -30,7 +32,10 @@ int PutFileRemote(const char *filename);
 int NumInputs(char *string);
 
 // Returns 0 if message sent and server reply are successful.
-int HandleRequest(int socket, char *cmdbuffer, char *msgbuffer);
+int HandleRequestls(int socket, char *cmdbuffer, char *msgbuffer);
+
+// Returns 0 if message is sucessfully sent.
+int SendMessage(int socket, char *cmdbuffer, char *msgbuffer);
 
 
 /////////////////////////////////////////////////////////////////////
@@ -39,16 +44,13 @@ int HandleRequest(int socket, char *cmdbuffer, char *msgbuffer);
 
 int main(int argc, char *argv[])
 {
-	int sockfd;							// Socket descripter
- 	struct sockaddr_in servAddr;		// Server address
-	unsigned short servPort;			// Server port
-	char *servIP;						// Server IP address (dotted)
-	char *string;						// String to send to server
-	unsigned int stringLen;				// Length of string
-	int bytesRecvd, totalBytesRecvd;	// Bytes to read in single recv() and total bytes read
-	int rv;								// Return value of functions
-	char cmdbuffer[CMDBUFSIZE];			// Buffer for command
-	char msgbuffer[RCVBUFSIZE];			// msgbuffer for string
+	int sockfd;													// Socket descripter
+ 	struct sockaddr_in serveraddress;		// Server address
+	unsigned short serverport;					// Server port
+	unsigned int stringlen;							// Length of string
+	char cmdbuffer[BUFSIZE];					// Buffer for command
+	char msgbuffer[BUFSIZE];					// msgbuffer for string
+	char *servIP;												// Server IP address (dotted)
 
 /*
 	// for testing main logic only, skipping socket creation
@@ -58,30 +60,28 @@ int main(int argc, char *argv[])
 */
 
 	// check for correct # of arguments (1 or 2)
-	if((argc < 2) ||(argc > 3))
-	{
+	if ((argc < 2) || (argc > 3)) {
 		fprintf(stderr, "Usage: %s <Server IP> [<Port>]\n", argv[0]);
-		exit(1);
+		return -1;
 	}
 
 	// Server IP
 	servIP = argv[1];
 
 	// Check if port is specified
-	if(argc == 3)
-		servPort = atoi(argv[2]);
-	else
-		servPort = 7; // 7 is a well know port for echo service
+	if (argc == 3) {
+		serverport = atoi(argv[2]);
+	} else {
+		serverport = 7; // 7 is a well know port for echo service
+	}
 
 	#ifdef DEBUG
 	printf("[DEBUG] Creating TCP socket...\n");
 	#endif
 
 	// Create a TCP socket
-	if((sockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
-	{
-		perror("socket()");
-		exit(1);
+	if ((sockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+		Die("Failed to create socket");
 	}
 
 	#ifdef DEBUG
@@ -89,20 +89,18 @@ int main(int argc, char *argv[])
 	#endif
 
 	// Construct the server address structure
-	memset(&servAddr, 0, sizeof(servAddr));
-	servAddr.sin_family		 = AF_INET;
-	servAddr.sin_addr.s_addr = inet_addr(servIP);	// convert to network byte
-	servAddr.sin_port		 = htons(servPort);		// convert to port
+	memset(&serveraddress, 0, sizeof(serveraddress));
+	serveraddress.sin_family		 = AF_INET;
+	serveraddress.sin_addr.s_addr = inet_addr(servIP);	// convert to network byte
+	serveraddress.sin_port		 = htons(serverport);			// convert to port
 
 	#ifdef DEBUG
 	printf("[DEBUG] Connecting to server %s...\n", servIP);
 	#endif
 
 	// Connect to the server
-	if( (rv = connect(sockfd, (struct sockaddr *) &servAddr, sizeof(servAddr))) < 0)
-	{
-		perror("connect()");
-		exit(1);
+	if (connect(sockfd, (struct sockaddr *) &serveraddress, sizeof(serveraddress)) < 0) {
+		Die("Failed to connect to server");
 	}
 
 	#ifdef DEBUG
@@ -110,48 +108,27 @@ int main(int argc, char *argv[])
 	#endif
 
 /*
-	// Need to do this differently
-	string = "connect\0";
-	stringLen = strlen(string);
-
-
-
-	#ifdef DEBUG
-	printf("--== Sending message '%s' to the server --==\n", string);
-	#endif
-
-	// Send a message to the server to connect.
-	if((rv = send(sockfd, string, stringLen, 0)) != stringLen)
-	{
-		//printf("send() sent a different number of bytes than expected.\nError: %i", rv);
-		perror("send()");
-		exit(1);
-	}
-
-	#ifdef DEBUG
-	printf("--== Sent message '%s' to the server --==\n", string);
-	#endif
-*/
-
-/*
 	#ifdef DEBUG
 	skip:
 	#endif
 */
 
-	// Main logic code
-	while(1)
-	{
-		printf("ft> ");
-		fgets(cmdbuffer, 100, stdin);
+	// Return value variable for functions
+	int rv = 0;
 
-		// replace '\n' at the end of cmdbuffer with '\0'.
+	// Main logic code
+	while (1)	{
+		printf("ft> ");
+		fgets(cmdbuffer, BUFSIZE, stdin);
+
+		// replace '\n' at the end of user input with '\0'.
 		char *pos;
-		if((pos=strchr(cmdbuffer, '\n')) != NULL)
+		if ((pos = strchr(cmdbuffer, '\n')) != NULL) {
 			*pos = '\0';
+		}
 
 		// NumInputs replaces the buffer used, so create a new buffer.
-		char pstring[CMDBUFSIZE];
+		char pstring[BUFSIZE];
 		strcpy(pstring, cmdbuffer);
 
 		// Check the number of inputs given by the user.
@@ -164,26 +141,21 @@ int main(int argc, char *argv[])
 
 		// This big if/else could be changed.
 		// 1 command input
-		if(rv == 1)
-		{
+		if (rv == 1) {
 			#ifdef DEBUG
 			printf("[DEBUG] 1 word input\n");
 			#endif
 
-			if(strcmp(cmdbuffer, "quit") == 0)
-			{
-				stringLen = strlen(cmdbuffer);
+			if (strcmp(cmdbuffer, "quit") == 0) {
+				stringlen = strlen(cmdbuffer);
 
 				#ifdef DEBUG
 				printf("--== Sending message '%s' to the server --==\n", cmdbuffer);
 				#endif
 
 				// Send the 'quit' message to the server.
-				if((rv = send(sockfd, cmdbuffer, stringLen, 0)) != stringLen)
-				{
-					//printf("send() sent a different number of bytes than expected.\nError: %i", rv);
-					perror("send(quit)");
-					exit(1);
+				if ((rv = send(sockfd, cmdbuffer, stringlen, 0)) != stringlen) {
+					Die("Failed to send 'quit' message to server");
 				}
 
 				#ifdef DEBUG
@@ -192,25 +164,18 @@ int main(int argc, char *argv[])
 
 				// Exit program.
 				break;
-			} 
-			else if(strcmp(cmdbuffer, "help") == 0)
-			{
+			} else if (strcmp(cmdbuffer, "help") == 0) {
 				HelpMessage();
-			} 
-			else if(strcmp(cmdbuffer, "ls") == 0)
-			{
-
+			} else if (strcmp(cmdbuffer, "ls") == 0) {
 				#ifdef DEBUG
 				printf("[DEBUG] ls command\n");				
 				#endif
 
-				stringLen = strlen(cmdbuffer);
+				stringlen = strlen(cmdbuffer);
 
 				// send ls command to server
-				if((rv = send(sockfd, cmdbuffer, stringLen, 0)) != stringLen)
-				{
-					printf("send() sent a different number of bytes than expected.\nError: %i", rv);
-					exit(1);
+				if ((rv = send(sockfd, cmdbuffer, stringlen, 0)) != stringlen) {
+					Die("Failed to send 'ls' message to server");
 				}
 
 				#ifdef DEBUG
@@ -223,37 +188,26 @@ int main(int argc, char *argv[])
 				#endif
 
 				// receive ls results from server
-				HandleRequest(sockfd, cmdbuffer, msgbuffer);
+				HandleRequestls(sockfd, cmdbuffer, msgbuffer);
 
 				#ifdef DEBUG
 				printf("--== Received results of '%s' from the server --==\n", cmdbuffer);
 				#endif
-			}
-			else if(strcmp(cmdbuffer, "clear") == 0)
-			{
+			} else if (strcmp(cmdbuffer, "clear") == 0) {
 				system("clear");
-			}
-			else 
-			{
+			} else {
 				printf("Invalid command.\n");
 				HelpMessage();
-			}
-		} // End of 1 command input.
-		// 2 command inputs.
-		else if(rv == 2)
-		{
+			} // End of 1 command input.
+		} else if (rv == 2) { // 2 command inputs.
 			// Basic format validation for option and 1 argument.
-			if(StartsWith(cmdbuffer, "get") && strstr(cmdbuffer, " "))
-			{
+			if ((StartsWith(cmdbuffer, "get") == 0) && strstr(cmdbuffer, " ")) {
 				#ifdef DEBUG
 				printf("[DEBUG] get <remote-file> command\n");
 				#endif			
 
 				// Send 'mkdir <directory-name>' message.
-
-			} 
-			else if(StartsWith(cmdbuffer, "put ") && strstr(cmdbuffer, " "))
-			{
+			} else if ((StartsWith(cmdbuffer, "put ") == 0) && strstr(cmdbuffer, " "))	{
 				#ifdef DEBUG
 				printf("[DEBUG] put <file-name> command\n");
 				#endif
@@ -261,17 +215,17 @@ int main(int argc, char *argv[])
 				// 1. Verify <file-name> exists
 
 				// Need to extract the file name from cmdbuffer..
-				if(FileExists("testfile"))
-				{
+				if (FileExists("testfile") == 0) {
 					#ifdef DEBUG
 					printf("[DEBUG] file name: %s does exist\n", cmdbuffer);
 					#endif
 
 					// 2. Send file to server through messages
-					if((rv = PutFileRemote("<file-name>")) < 0)
+					if (PutFileRemote("<file-name>")) {
 						printf("put <file-name> failed with error: %i", rv);
-					else 
+					} else { 
 						printf("%s was successful\n", cmdbuffer);
+					}
 				}
 
 				// Send 'put <file-name>' message to server.
@@ -280,39 +234,30 @@ int main(int argc, char *argv[])
 
 				// Output that the message transfer is complete.
 
-			}
-			else if(StartsWith(cmdbuffer, "cd") && strstr(cmdbuffer, " "))
-			{
+			} else if ((StartsWith(cmdbuffer, "cd") == 0) && strstr(cmdbuffer, " ")) {
 				#ifdef DEBUG
 				printf("[DEBUG] cd <directory> command\n");
 				#endif
 
 				// Send 'cd <directory-name' message to server.
+				SendMessage(sockfd, cmdbuffer, msgbuffer);
 
-			}
-			else if(StartsWith(cmdbuffer, "mkdir") && strstr(cmdbuffer, " "))
-			{
+			} else if ((StartsWith(cmdbuffer, "mkdir") == 0) && strstr(cmdbuffer, " ")) {
 				#ifdef DEBUG
 				printf("[DEBUG] mkdir <directory-name> command\n");
 				#endif
 
 				// Send 'mkdir <directory-name>' message to server.
 
-			}
-			else 
-			{
+			} else {
 				printf("Invalid command.\n");
 				HelpMessage();
-			}
-		} // End of 2 command input.
-		else
-		{
+			} // End of 2 command input.
+		} else {
 			printf("Invalid command.\n");
 			HelpMessage();
 		}
-
 	} // End while loop.
-
 } // End main.
 
 
@@ -320,15 +265,11 @@ int main(int argc, char *argv[])
 // Function implementations.
 /////////////////////////////////////////////////////////////////////
 
-int StartsWith(const char *string, const char *substring)
-{
-	if(strncmp(string, substring, strlen(substring)) == 0)
-		return 1;
-	return 0;
+int StartsWith(const char *string, const char *substring) {
+	return strncmp(string, substring, strlen(substring));
 }
 
-void HelpMessage()
-{
+void HelpMessage() {
 	printf("Commands are:\n\n");
 	printf("ls:\t\t print a listing of the contents of the current directory\n");
 	printf("get <remote-file>:\t\t retrieve the <remote-file> on the server and store it in the current directory\n");
@@ -337,30 +278,54 @@ void HelpMessage()
 	printf("mkdir <directory-name>:\t\t create a new sub-directory named <directory-name>\n");
 }
 
-int HandleRequest(int socket, char *cmdbuffer, char *msgbuffer)
-{
-	int rv = 0;
-	unsigned int stringLen = strlen(cmdbuffer);
+int FileExists(const char *filename) {
+	return access(filename, F_OK);
+}
+
+int PutFileRemote(const char *filename) {
+	return 0;
+}
+
+int NumInputs(char *string) {
+	// send ls command
+	#ifdef DEBUG
+	printf("[DEBUG] inside NumInputs. String passed was: %s\n", string);
+	#endif			
+
+	int numsubstrings = 0;
+	char *token = strtok(string, " ");
+	while (token) {
+		#ifdef DEBUG
+		printf("[DEBUG] token: %s\n", token);
+		#endif		
+
+		numsubstrings++;
+		token = strtok("\0", " ");
+	}
+	
+	return numsubstrings;
+}
+
+int HandleRequestls(int socket, char *cmdbuffer, char *msgbuffer) {
+	unsigned int stringlen = strlen(cmdbuffer);
 
 	// Send command to server
-	if((rv = send(socket, cmdbuffer, stringLen, 0)) != stringLen)
-	{
-		//printf("send() sent a different number of bytes than expected.\nError: %i", rv);
-		perror("(HandleRequest: send(cmdbuffer)");
-		exit(1);
+	if (send(socket, cmdbuffer, stringlen, 0) != stringlen) {
+		Die("Failed to send message");
 	}
 
 	int bytesRecvd = 0;
 
 	// Read results from server until a null terminator is found
-	while(1)
-	{
-		if((bytesRecvd = recv(socket, msgbuffer, RCVBUFSIZE, 0)) < 0)
-			perror("HandleRequest: recv(msgbuffer");
+	while (1) {
+		if((bytesRecvd = recv(socket, msgbuffer, BUFSIZE, 0)) < 0) {
+			Die("Failed to receive message");
+		}
 
 		// Check if it is the last message
-		if(msgbuffer[0] == '\0')
+		if (msgbuffer[0] == '\0') {
 			break;
+		}
 
 		// Output the server results
 		printf("%s", msgbuffer);
@@ -372,38 +337,13 @@ int HandleRequest(int socket, char *cmdbuffer, char *msgbuffer)
 	return 0;
 }
 
-int FileExists(const char *filename)
-{
-	if(access(filename, F_OK) != -1) 
-	    return 1;
-	// File does not exist
-	return 0;
-}
+int SendMessage(int socket, char *cmdbuffer, char *msgbuffer) {
+	unsigned int stringlen = strlen(cmdbuffer);
 
-int PutFileRemote(const char *filename)
-{
-	return 0;
-}
-
-int NumInputs(char *string)
-{
-	// send ls command
-	#ifdef DEBUG
-	printf("[DEBUG] inside NumInputs. String passed was: %s\n", string);
-	#endif			
-
-	int numsubstrings = 0;
-	char *token = strtok(string, " ");
-	while(token)
-	{
-
-		#ifdef DEBUG
-		printf("[DEBUG] token: %s\n", token);
-		#endif		
-
-		numsubstrings++;
-		token = strtok(NULL, " ");
+	// Send command to server
+	if (send(socket, cmdbuffer, stringlen, 0) != stringlen) {
+		Die("Failed to send message");
 	}
-	
-	return numsubstrings;
+
+	return 0;
 }
